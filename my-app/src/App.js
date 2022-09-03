daf39ef68a6a28ef819f0bdb073999baf5364a6e
Vadym <stevarkmidav@gmail.com>
2022-09-03 17:30:44 +0300

    React test 2.0

---
+++ my-app/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="wrapper">
+      <div className="box">Card</div>
+      <div className="box">Card</div>
+      <div className="box">Card</div>
+      <div className="box">Card</div>
+    </div>
+  );
+}
+
+
+
+export default App;
