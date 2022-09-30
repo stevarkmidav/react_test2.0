916f4404c27b3470a619489adc7d3b816635547d
Vadym <stevarkmidav@gmail.com>
2022-09-30 18:18:03 +0300

    making mew branch

---
+++ my-app/src/components/menuList.js
@@ -0,0 +1,20 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+import { listItem } from './listItem';
+
+
+
+export function menuList () {
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
+        {Array.from(Array(6)).map((_, index) => (
+          <Grid item xs={2} sm={4} md={4} key={index}>
+            {listItem}
+          </Grid>
+        ))}
+      </Grid>
+    </Box>
+  )
+}
